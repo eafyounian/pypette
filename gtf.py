98e551d5ced834cab4f81db5d37cc4554f7bfcc4
annalam <annalam@users.noreply.github.com>
2015-04-27 15:45:39 +0300

    Fixed broken gtf.py subroutines.

--- gtf.py
+++ gtf.py
@@ -19,7 +19,10 @@ import sys, docopt, re, os
 from pypette import zopen, shell, revcomplement, info, error, shell_stdout
 from sam import read_sam
 
-
+human_chr = ['%d' % c for c in range(1, 23)] + ['X', 'Y']
+human_chr = set(human_chr + ['chr%s' % c for c in human_chr])
+ensembl_reasonable_gene_types = ['protein_coding', 'processed_transcript',
+	'pseudogene', 'lincRNA']
 
 
 ###################
@@ -32,21 +35,19 @@ def gtf_to_gene_bed(gtf_path):
 	
 	gtf_file = zopen(gtf_path)
 	for line in gtf_file:
-		tokens = line.split('\t')
-		if not tokens[2] == 'exon': continue
-		if tokens[1] in ['nonsense_mediated_decay', 'retained_intron']: continue
+		if line.startswith('#'): continue
+		c = line.rstrip('\n').split('\t')
+		if not c[0] in human_chr: continue
+		if not c[1] in ensembl_reasonable_gene_types: continue
+		if c[2] != 'exon': continue
 		
-		chr = tokens[0]
-		if not chr.startswith('chr'):
-			chr = 'chr' + chr
-			
-		pos = (int(tokens[3]), int(tokens[4]))
-		strand = tokens[6]
+		chr, start, end, strand = c[0], int(c[3]), int(c[4]), c[6]
+		if not chr.startswith('chr'): chr = 'chr' + chr
 		
-		m = re.search('gene_id "(.+?)"', line)
+		m = re.search(r'gene_id "(.+?)"', line)
 		gene_id = m.group(1)
 		
-		m = re.search('gene_name "(.+?)"', line)
+		m = re.search(r'gene_name "(.+?)"', line)
 		gene_name = m.group(1)
 		
 		exons = gene_exons.setdefault(gene_id, [])
@@ -55,18 +56,15 @@ def gtf_to_gene_bed(gtf_path):
 		gene_id_to_name[gene_id] = gene_name
 		
 	for gene_id, exons in gene_exons.iteritems():
-		if not all([exon[0] == exons[0][0] for exon in exons]):
+		if not all(exon[0] == exons[0][0] for exon in exons):
 			info('Chromosome confusion detected.')
-		if not all([exon[1] == exons[0][1] for exon in exons]):
+		if not all(exon[1] == exons[0][1] for exon in exons):
 			info('Strand confusion detected.')
 		
-		pos = (min([exon[2] for exon in exons]),
-			max([exon[3] for exon in exons]))
-		
-		print('%s\t%d\t%d\t%s (%s)\t\t%s' % (exons[0][0], pos[0] - 1, pos[1],
+		start, end = min(ex[2] for ex in exons), max(ex[3] for ex in exons)
+		print('%s\t%d\t%d\t%s (%s)\t\t%s' % (exons[0][0], start - 1, end,
 			gene_id_to_name[gene_id], gene_id, exons[0][1]))
 	
-	gtf_file.close()
 	
 
 
@@ -86,40 +84,29 @@ def gtf_to_transcript_bed(gtf_path):
 	gtf_file = zopen(gtf_path)
 	for line in gtf_file:
 		if line.startswith('#'): continue
-		tokens = line.split('\t')
-		if not tokens[2] == 'exon': continue
-		if tokens[1] in ['nonsense_mediated_decay', 'retained_intron']: continue
+		c = line.rstrip('\n').split('\t')
+		if not c[0] in human_chr: continue
+		if not c[1] in ensembl_reasonable_gene_types: continue
+		if c[2] != 'exon': continue
 		
-		chr = tokens[0]
-		if not chr.startswith('chr'):
-			chr = 'chr' + chr
-			
-		pos = (int(tokens[3]), int(tokens[4]))
-		strand = tokens[6]
+		chr, start, end, strand = c[0], int(c[3]), int(c[4]), c[6]
+		if not chr.startswith('chr'): chr = 'chr' + chr
 		
-		m = re.search('transcript_id "(.+?)"', line)
+		m = re.search(r'transcript_id "(.+?)"', line)
 		tx_id = m.group(1)
 		
-		m = re.search('gene_name "(.+?)"', line)
+		m = re.search(r'gene_name "(.+?)"', line)
 		gene_name = m.group(1)
 		
 		exons = tx_exons.setdefault(tx_id, [])
-		exons.append((chr, strand, pos[0], pos[1]))
-		
+		exons.append((chr, strand, start, end))
 		tx_id_to_name[tx_id] = gene_name
 		
 	for tx_id, exons in tx_exons.iteritems():
-		#if not all([exon[0] == exons[0][0] for exon in exons]):
-		#if not all([exon[1] == exons[0][1] for exon in exons]):
-	#		print('Strand confusion detected.')
-		
-		pos = (min([exon[2] for exon in exons]),
-			max([exon[3] for exon in exons]))
-		
-		print('%s\t%d\t%d\t%s (%s)\t\t%s' % (exons[0][0], pos[0] - 1, pos[1],
+		start, end = min(ex[2] for ex in exons), max(ex[3] for ex in exons)
+		print('%s\t%d\t%d\t%s (%s)\t\t%s' % (exons[0][0], start - 1, end,
 			tx_id_to_name[tx_id], tx_id, exons[0][1]))
 	
-	gtf_file.close()
 
 
 
@@ -134,16 +121,17 @@ def gtf_to_transcript_bed(gtf_path):
 
 def gtf_to_exon_bed(gtf_path):
 	for line in zopen(gtf_path):
-		c = line.split('\t')
-		if not c[2] == 'exon': continue
-		if c[1] in ['nonsense_mediated_decay', 'retained_intron']: continue
-		if c[0].startswith('HSCHR'): continue
-		if c[0].startswith('GL'): continue
-		if c[0].startswith('HG'): continue
+		if line.startswith('#'): continue
+		c = line.rstrip('\n').split('\t')
+		if not c[0] in human_chr: continue
+		if not c[1] in ensembl_reasonable_gene_types: continue
+		if c[2] != 'exon': continue
+
+		chr, start, end = c[0], int(c[3]), int(c[4])
+		if not chr.startswith('chr'): chr = 'chr' + chr
+		gene_id = re.search(r'gene_id "(.+?)"', line).group(1)
 		
-		gene = re.search('gene_id "(.+?)"', line).group(1)
-		chr = 'chr'+c[0] if not c[0].startswith('chr') else c[0]
-		print('%s\t%d\t%s\t%s\t\t%s' % (chr, int(c[3])-1, c[4], gene, c[6]))
+		print('%s\t%d\t%s\t%s\t\t%s' % (chr, start-1, end, gene_id, c[6]))
 
 
 
@@ -158,31 +146,34 @@ def gtf_to_exon_bed(gtf_path):
 #############################
 
 def gtf_to_composite_exon_bed(bed_path):
-	
 	genes = {}
 	
-	bed_file = open(bed_path)
+	bed_file = zopen(bed_path)
 	for line in bed_file:
-		chr, start, end, gene_id = line[:-1].split('\t')[:4]
-		start = int(start)
-		end = int(end)
+		if line.startswith('#'): continue
+		c = line.rstrip('\n').split('\t')
+		if not c[0] in human_chr: continue
+		if not c[1] in ensembl_reasonable_gene_types: continue
+		if c[2] != 'exon': continue
+
+		chr, start, end = c[0], int(c[3]), int(c[4])
+		if not chr.startswith('chr'): chr = 'chr' + chr
+		gene_id = re.search(r'gene_id "(.+?)"', line).group(1)
 		
-		gene = genes.setdefault(gene_id, [chr, [(start, end)]])
-		if chr != gene[0]:
-			print('ERROR: Chromosome mismatch.')
+	 	gene = genes.setdefault(gene_id, [chr, [(start, end)]])
+	 	if chr != gene[0]: error('Chromosome mismatch.')
 		
-		exons = gene[1]
-		overlapping = [ex for ex in exons if end >= ex[0] and start <= ex[1]]
-		disjoint = [ex for ex in exons if not (end >= ex[0] and start <= ex[1])]
-		
-		disjoint.append((min([start] + [ex[0] for ex in overlapping]),
-			max([end] + [ex[1] for ex in overlapping])))
-		gene[1] = disjoint
+	 	exons = gene[1]
+	 	overlapping = [ex for ex in exons if end >= ex[0] and start <= ex[1]]
+	 	disjoint = [ex for ex in exons if not (end >= ex[0] and start <= ex[1])]
+	 	disjoint.append((min([start] + [ex[0] for ex in overlapping]),
+	 		max([end] + [ex[1] for ex in overlapping])))
+	 	gene[1] = disjoint
 		
 	for gene_id, gene in genes.iteritems():
-		exons = gene[1]
-		for ex in exons:
-			print('%s\t%d\t%d\t%s' % (gene[0], ex[0], ex[1], gene_id))
+	 	exons = gene[1]
+	 	for ex in exons:
+	 		print('%s\t%d\t%d\t%s' % (gene[0], ex[0] - 1, ex[1], gene_id))
 
 
 
@@ -215,10 +206,10 @@ if __name__ == '__main__':
 		gtf_to_gene_bed(args['<gtf_file>'])
 	elif args['to'] and args['transcript'] and args['bed']:
 		gtf_to_transcript_bed(args['<gtf_file>'])
-	elif args['to'] and args['exon'] and args['bed']:
-		gtf_to_exon_bed(args['<gtf_file>'])
 	elif args['to'] and args['composite'] and args['exon'] and args['bed']:
 		gtf_to_composite_exon_bed(args['<gtf_file>'])
+	elif args['to'] and args['exon'] and args['bed']:
+		gtf_to_exon_bed(args['<gtf_file>'])
 	elif args['cleanup'] and args['ensembl']:
 		cleanup_ensembl(args['<gtf_file>'])
 
